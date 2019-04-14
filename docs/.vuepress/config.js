34ce2266a6819d76870562f77114e88d2526097b
Jürgen Ratzenböck <juergen.ratzenboeck@gmail.com>
2019-04-14 16:35:18 +0200

    Add cookie consent

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,29 @@
+const cookieConsent =
+  "window.addEventListener('load', function() { window.cookieconsent.initialise({ palette: { popup: { background: '#252e39' }, button: { background: '#14a7d0' } }, content: { message: 'Diese Webseite verwendet Cookies für Analyse- und Messungszwecke. Durch die Nutzung dieser Webseite stimmen Sie der Nutzung von Cookies zu.', dismiss: 'Ok, verstanden', link: 'Mehr erfahren' } });});";
+
 module.exports = {
   title: 'jratzenboeck loves sports',
   description:
     'Mein persönlicher Sport Blog mit Themen zu Motivation, Training, Wettkampf, Regeneration und vielem mehr.',
-  head: [['meta', { name: 'robots', content: 'index,follow' }]],
+  head: [
+    ['meta', { name: 'robots', content: 'index,follow' }],
+    [
+      'link',
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href: '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css'
+      }
+    ],
+    [
+      'script',
+      {
+        src: '//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js',
+        defer: true
+      }
+    ],
+    ['script', {}, cookieConsent]
+  ],
   themeConfig: {
     primaryColor: '#00838f',
     navbarItems: [
